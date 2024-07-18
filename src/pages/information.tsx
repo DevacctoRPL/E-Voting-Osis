d390bade4f90056f64dd09693755a9b7d62f808c
adeleeeeyyyy <mitsuhaadly@gmail.com>
2024-07-18 15:55:26 +0700

    mantap

--- src/pages/information.tsx
+++ src/pages/information.tsx
@@ -4,15 +4,20 @@ import Arrow from '/arrow.png';
 // Styling Component
 // import Head from "../styling-components/header";
 import Footer from '../styling-components/footer';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Information: React.FC = () => {
+  const navigate = useNavigate();
+
+  const back = () => {
+    navigate('/list-kandidat'); 
+  };
   return (
     <>
       <body className='mb-[10rem] font-poppins'>
-        <Link className="w-[50px] h-[50px] rounded-full bg-gradient-to-r from-merah-penus to-merah-gelap-penus mt-5 ml-5 flex items-center justify-center relative" to={"/list-kandidat"}>
+        <div onClick={back} className="w-[50px] h-[50px] rounded-full bg-gradient-to-r from-merah-penus to-merah-gelap-penus mt-5 ml-5 flex items-center justify-center relative">
           <img src={Arrow} alt="" className="w-[30px] h-[30px]" />
-        </Link>
+        </div>
         <div id="nomor" className="flex flex-col items-start justify-center h-full">
           <h1 className="z-[-1] text-[60rem] mt-[500px] text-[#000000] opacity-40 font-extrabold absolute">01</h1>
         </div>
