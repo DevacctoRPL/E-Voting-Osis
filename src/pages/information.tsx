615608d0c595ad82439f90ffc53337bf22dd8e76
adeleeeeyyyy <mitsuhaadly@gmail.com>
2024-07-18 10:42:00 +0700

    Information page insyaAllah done

--- src/pages/information.tsx
+++ src/pages/information.tsx
@@ -9,7 +9,7 @@ const Information: React.FC = () => {
   return (
     <>
       <body className='mb-[10rem] font-poppins'>
-        <div className="w-[50px] h-[50px] rounded-full bg-merah-penus mt-5 ml-5 flex items-center justify-center relative">
+        <div className="w-[50px] h-[50px] rounded-full bg-gradient-to-r from-merah-penus to-merah-gelap-penus mt-5 ml-5 flex items-center justify-center relative">
           <img src={Arrow} alt="" className="w-[30px] h-[30px]" />
         </div>
         <div id="nomor" className="flex flex-col items-start justify-center h-full">
@@ -26,17 +26,49 @@ const Information: React.FC = () => {
               <h1 className="text-[4rem] text-wrap ">Dilon donovan Damaswara Putra</h1>
               <p className='flex justify-center -ml-[20rem]'> Dillon Juara Indonesia Dapat Wifi 2 GBPS</p>
             </div>
-            <div id='visi' className='w-[7rem] h-[1.5rem] bg-merah-penus -ml-[10rem] mt-[2rem]'>
-              <h1 className='font-semibold text-white inline-block align-middle text-center'>Visi</h1>
-              <ul className='list-disc text-white '>
-                <li>AkAN Mewmberikan sprei gratis setiap hari</li>
-                <li>ayam potojh</li>
-                <li>ayam potojh</li>
-                <li>ayam potojh</li>
-                <li>ayam potojh</li>
-              </ul>
+            <div id="visi-wrapper">
+              <div id='visi' className='w-[5rem] h-[1.5rem] bg-merah-penus -ml-[10rem] mt-[2rem]'>
+                <h1 className='font-semibold text-white inline-block align-middle text-center ml-5'>Visi</h1>
+                <div className='w-[50rem] ml-[1rem]'>
+                  <ul className='list-disc text-white mt-5'>
+                    <li>AkAN Mewmberikan sprei gratis setiap hari</li>
+                    <li>ayam potojh</li>
+                    <li>ayam potojh</li>
+                    <li>ayam potojh</li>
+                    <li>ayam potojh</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div id="misi-wrapper">
+              <div id='misi' className='w-[5rem] h-[1.5rem] bg-merah-penus -ml-[10rem] mt-[10rem]'>
+                <h1 className='font-semibold text-white inline-block align-middle text-center ml-5'>Misi</h1>
+                <div className='w-[50rem] ml-[1rem]'>
+                  <ul className='list-disc text-white mt-5'>
+                    <li>AkAN Mewmberikan sprei gratis setiap hari</li>
+                    <li>ayam potojh</li>
+                    <li>ayam potojh</li>
+                    <li>ayam potojh</li>
+                    <li>ayam potojh</li>
+                  </ul>
+                </div>
+              </div>
             </div>
 
+            <div id="proker-wrapper">
+              <div id='proker' className='w-[10rem] h-[1.5rem] bg-merah-penus -ml-[10rem] mt-[10rem]'>
+                <h1 className='font-semibold text-white inline-block align-middle text-center ml-5'>Program kerja</h1>
+                <div className='w-[50rem] ml-[1rem]'>
+                  <ul className='list-disc text-white mt-5'>
+                    <li>AkAN Mewmberikan sprei gratis setiap hari</li>
+                    <li>ayam potojh</li>
+                    <li>ayam potojh</li>
+                    <li>ayam potojh</li>
+                    <li>ayam potojh</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
           </div>
         </div>  
       </body>
